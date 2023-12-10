4dbfb705f72175b1b842030027da29acbd9864fd
cyblogerz <pranavajay2002@gmail.com>
2023-12-10 19:42:07 +0530

    [style]: Updated pomo widgetstyle sheet

--- src/apps/timer/widgets/PomodoroWidget/pomodoro.tsx
+++ src/apps/timer/widgets/PomodoroWidget/pomodoro.tsx
@@ -2,6 +2,7 @@
 import { Button } from '../../../../components/ui/button'
 import {  RotateCcw } from 'lucide-react'
 import Draggable from 'react-draggable';
+import './styles/main.css';
 
 
 export const PomodoroWidget = () => {
@@ -17,18 +18,12 @@ export const PomodoroWidget = () => {
   
    p-6 
     rounded-2xl  h-[35] w-[35]
-    bg-[#FFF5EE] border-0
+    bg-[#FFF5EE] border-2 border-[#572D1C]
    flex flex-col items-center justify-center
    ">
-    {/* <div className='flex flex-row gap-x-4 pb-10'>
-      <Button size={'lg'} variant={'default'}>Pomodoro</Button>
-      <Button size={'lg'} variant={'ghost'}>Short break</Button>
-      <Button size={'lg'} variant={'ghost'}>Long Break</Button>
-
-    </div> */}
     
     <p className="text-xl text-[#572D1C]">Pomodoro</p>
-  <div className="relative text-6xl text-[#F7B88B]  font-bold pt-2">
+  <div className="relative text-6xl text-[#F7B88B] pomo-text-stroke  font-bold pt-2">
     25:00
   </div>
   <div className='flex flex-row gap-x-4 pt-2'>
